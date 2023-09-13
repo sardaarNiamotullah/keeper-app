ae930976d45630061f4d18d41510dc847c5957c2
Sardaar Niamotullah <sniamot@localhost.local>
2023-09-14 03:32:47 +0600

    Initial Commit with some basic header, footer

---
+++ src/components/Note.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Note() {
+    return <div className="note" draggable="true">
+        <h1>Title</h1>
+        <p>Hi, How hare you?</p>
+    </div>
+}
